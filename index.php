0943d542451007e238c09cc72258e13bacfddb14
Dishant Aarak <57016293+dishant-codes@users.noreply.github.com>
2021-08-01 08:51:15 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Birthday Card by Dishant</title>
+    <title>Birthday Card From Dishant</title>
     <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="area">
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
     <div class="card">
-      <div class="back"></div>
-      <div class="front">
-        <div class="imgset">
-          <img
-            width="100%"
-            src="https://1.bp.blogspot.com/-Mgj9-rbs65E/XfMoPSD5gtI/AAAAAAAAURk/NBokE2gSS2cTSJ2em5lZ5hJDuTtRN7UVwCLcBGAsYHQ/s1600/2713997.png"
-          />
+        <div class="back"></div>
+        <div class="front">
+            <div class="imgset">
+                <img width="100%"
+                    src="https://1.bp.blogspot.com/-Mgj9-rbs65E/XfMoPSD5gtI/AAAAAAAAURk/NBokE2gSS2cTSJ2em5lZ5hJDuTtRN7UVwCLcBGAsYHQ/s1600/2713997.png" />
+            </div>
         </div>
-      </div>
-      <div class="text-container">
-        <p id="head">Happy Birthday *****!</p>
-        <p>
-          I hope your special day will bring you lots of happiness, love, and
-          fun. You deserve them a lot. Enjoy!
-        </p>
-        <p>Hope your day goes great!</p>
-      </div>
+        <div class="text-container">
+            <p id="head">Happy Birthday *****!</p>
+            <p>
+                I hope your special day will bring you lots of happiness, love, and
+                fun. You deserve them a lot. Enjoy!
+            </p>
+            <p>Hope your day goes great!</p>
+        </div>
     </div>
-  </body>
+</body>
+
 </html>
